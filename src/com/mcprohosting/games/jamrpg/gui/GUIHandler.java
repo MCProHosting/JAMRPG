20ce520f9f38fbcd555bee9a10fee4bd1c256943
MattSalsamendi <matt@mcprohosting.com>
2013-09-08 17:30:06 -0400

    Added game?

--- src/com/mcprohosting/games/jamrpg/gui/GUIHandler.java
+++ src/com/mcprohosting/games/jamrpg/gui/GUIHandler.java
@@ -1,10 +1,19 @@
 package com.mcprohosting.games.jamrpg.gui;
 
 import com.mcprohosting.games.jamrpg.JAMRPG;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
 
 public class GUIHandler {
 	public static void init() {
-		MasterDisplay display = new MasterDisplay(JAMRPG.getShortTitle() + " v" + JAMRPG.getVersion(), 800, 600);
-		display.start();
+		try {
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new Game(JAMRPG.getShortTitle() + " v" + JAMRPG.getVersion()));
+			appgc.setDisplayMode(800, 600, false);
+			appgc.start();
+		} catch (SlickException e) {
+			System.out.println("Error while initializing slick engine");
+			e.printStackTrace();
+		}
 	}
 }
--- src/com/mcprohosting/games/jamrpg/gui/Game.java
+++ src/com/mcprohosting/games/jamrpg/gui/Game.java
@@ -1,4 +1,33 @@
 package com.mcprohosting.games.jamrpg.gui;
 
-public class Game {
+import org.newdawn.slick.*;
+import org.newdawn.slick.font.effects.ColorEffect;
+
+public class Game extends BasicGame {
+	public Game(String title) {
+		super(title);
+	}
+
+	public void init(GameContainer gc) throws SlickException {
+
+	}
+
+	public void update(GameContainer gc, int i) throws SlickException {
+
+	}
+
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		try {
+			final java.awt.Font javaFont = java.awt.Font.createFont(java.awt.Font.TRUETYPE_FONT, org.newdawn.slick.util.ResourceLoader.getResourceAsStream("/src/res/fonts/Cardinal.ttf"));
+			UnicodeFont uniFont = new UnicodeFont(javaFont);
+			uniFont.addAsciiGlyphs();
+			uniFont.getEffects().add(new ColorEffect());
+		} catch (Exception e) {
+			System.out.println("Error while loading font");
+			e.printStackTrace();
+		}
+
+		//Font font = new Font(uniFont)
+		g.drawString("Love you James <3", 50, 50);
+	}
 }
